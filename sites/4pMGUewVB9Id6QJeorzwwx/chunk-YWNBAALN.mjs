62143408ca83500b8404c6a466654058a05d3471
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2024-12-13 11:04:04 +0200

    Update chunk-YWNBAALN.mjs

--- sites/4pMGUewVB9Id6QJeorzwwx/chunk-YWNBAALN.mjs
+++ sites/4pMGUewVB9Id6QJeorzwwx/chunk-YWNBAALN.mjs
@@ -1,10 +1,10 @@
 function t(a, e) {
   return {
-    description: "PINKY - A cute little dog on the solana blockchain",
+    description: "PING - A cute little dog on the solana blockchain",
     favicon:
       "https://framerusercontent.com/assets/23xKGpDxJQCeieLXwuGtTaRA.webp",
     robots: "max-image-preview:large",
-    title: "PINKY - A cute little dog on the solana blockchain",
+    title: "PING - A cute little dog on the solana blockchain",
   };
 }
 export { t as a };
